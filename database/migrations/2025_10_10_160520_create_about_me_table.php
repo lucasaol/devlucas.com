210f1f51c310177f9a75c2adb5402e54d1ef81b4
Lucas Andrade <eudevlucas@gmail.com>
2025-10-10 13:50:29 -0300

    db: Model + migration conteúdo Sobre mim

---
+++ database/migrations/2025_10_10_160520_create_about_me_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('about_me', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('caption', 50)->nullable();
+            $table->string('avatar', 255);
+            $table->string('introduction', 255)->nullable();
+            $table->text('content');
+            $table->string('picture', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('about_me');
+    }
+};
