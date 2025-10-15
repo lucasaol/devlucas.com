b10a33b5ef5417df7cd2d60ca60de412caf68f52
Lucas Andrade <eudevlucas@gmail.com>
2025-10-15 18:17:31 -0300

    feat: sidebar

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,15 @@
 
 use App\Livewire\Pages\Home;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', Home::class)->name('index');
+
+
+Route::get('image/{path}', function ($path) {
+    $disk = Storage::disk('local');
+    if (!$disk->exists($path)) {
+        abort(404);
+    }
+    return response()->file($disk->path($path));
+})->where('path', '.*')->name('image');
