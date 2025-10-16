6d0a6504533a07ef670226b2431ef9403c9e5a81
Lucas Andrade <eudevlucas@gmail.com>
2025-10-15 22:43:30 -0300

    fix: finaliza sidebar

--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -5,14 +5,13 @@
     x-data="{ open: false }"
 >
     <div class="flex-shrink-0 px-8 py-4 flex flex-row items-center justify-between bg-primary-500 dark:bg-dark-500">
-
         <a href="{{ route('index') }}" class="md:m-auto">
             <span>&lt;</span>
             {{ config('app.name') }}
             <span>/&gt;</span>
         </a>
 
-        <button class="rounded-lg md:hidden rounded-lg focus:outline-none focus:shadow-outline" @click="open = !open">
+        <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline" @click="open=!open">
             <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
                 <path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
                 <path x-show="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -20,11 +19,9 @@
         </button>
     </div>
 
-
-    <div :class="{'block': open, 'hidden': !open}" class="bg-primary-500 dark:bg-dark-500 text-light-900 flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto ">
+    <div :class="{'block': open, 'hidden': !open}" class="bg-primary-500 dark:bg-dark-500 text-light-900 flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">
 
         <div class="flex flex-col items-center space-y-4">
-
             <div class="p-4 flex flex-col items-center">
                 <livewire:layout.dark-mode-toggle />
             </div>
