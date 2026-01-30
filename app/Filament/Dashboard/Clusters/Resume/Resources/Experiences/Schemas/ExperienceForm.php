c809cd90c4a55aa42d8dd41a9c9ab4ead7f70786
Lucas Andrade <eudevlucas@gmail.com>
2026-01-30 17:36:59 -0300

    fix: update textarea size

--- app/Filament/Dashboard/Clusters/Resume/Resources/Experiences/Schemas/ExperienceForm.php
+++ app/Filament/Dashboard/Clusters/Resume/Resources/Experiences/Schemas/ExperienceForm.php
@@ -43,7 +43,8 @@ class ExperienceForm
                     ]),
                 Textarea::make('description')
                     ->required()
-                    ->columnSpanFull(),
+                    ->columnSpanFull()
+                    ->rows(8),
             ]);
     }
 }
